ca2b272b1478cfb75971d13d63900f7ca956d276
26priti <30688326+26priti@users.noreply.github.com>
2017-08-05 10:55:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title:'Article one |Priti Jadhav',
+    heading:'article one',
+    date:'5 aug 2017',
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
