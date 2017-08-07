a1eec27eaee5644e7219ac38cee39d950e82f7e9
26priti <30688326+26priti@users.noreply.github.com>
2017-08-07 20:38:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,17 @@ console.log('Loaded!');
 //change the text of main text div
 var element = document.getElementById("main.text");
 element.innerHTML="new Value";
+
+
 //make image move
 var img = document.getElementById("madi");
 var marginleft=0;
+
 function moveRight(){
   marginleft = marginleft+1;
   img.style.marginleft = marginleft+'px';
 }
+
 img.onclick = function(){
     var interval=  setInterval(moveRight,  50);
 };
