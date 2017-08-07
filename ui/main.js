55e54ed1e2cb4f8ec6c25b3f91e3cd6140b489a3
26priti <30688326+26priti@users.noreply.github.com>
2017-08-07 19:51:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var img = document.getElementById('madi');
 var marginleft=0;
 function moveright(){
   marginleft=marginleft+1;
-  img.style.marginleft+px;
+  img.style.marginleft+'px';
 }
 img.onclick = function(){
     var interval=  setInterval(moveright,  50);
