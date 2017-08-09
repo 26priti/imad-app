11870c6adc9e70094463edab3a0fd7a8d0a9083f
26priti <30688326+26priti@users.noreply.github.com>
2017-08-09 16:21:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,79 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles= {
- 'article-one': {
-    title:'Article one|Priti Jadhav',
-    heading:'article one',
-    date:'5 aug 2017',
-    content: ` <p>
-                This is my first article. This is the first page of my article. This is the content of the article. This is my first article. This is the first page of my article. This is the content of the article. This is my first article. This is the first page of my article. This is the content of the article.
-            </p>
-              <p>
-                This is my first article. This is the first page of my article. This is the content of the article. This is my first article. This is the first page of my article. This is the content of the article. This is my first article. This is the first page of my article. This is the content of the article.
-            </p>
-              <p>
-                This is my first article. This is the first page of my article. This is the content of the article. This is my first article. This is the first page of my article. This is the content of the article. This is my first article. This is the first page of my article. This is the content of the article.
-            </p>`
-},
- 'article-two': {
-    title:'Article two|Priti Jadhav',
-    heading:'article one',
-    date:'5 aug 2017',
-    content: ` <p>
-                This is my second article. This is the first page of my article. This is the content of the article.
-                </p>`
- },
- 'article-three': {
-    title:'Article three|Priti Jadhav',
-    heading:'article three',
-    date:'10 aug 2017',
-    content: ` <p>
-                This is my third article. This is the first page of my article. This is the content of the article. >`
- }
-};
-function createTemplate (data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-var htmlTemplate=`
- <html>
-    <head>
-        ${title}
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="ui/style.css" rel="stylesheet" />     
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href='/'>Home</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-            ${content}
-        </div>
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
-}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/:articleName', function(req,res){
-    //articleName=articleone
-    
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
