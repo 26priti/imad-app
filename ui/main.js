56fea1cf52b653dd46988cb6355f4a2f0879d8d0
26priti <30688326+26priti@users.noreply.github.com>
2017-08-09 18:38:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,11 +27,14 @@ var nameInput = document.getElementById('name');
 var name= nameInput.value;
 var submit= document.getElementById('submit_btn');
 submit.onclick = function(){
+    
     //make request to server and send the name
+    
     //capture names and render list
+
     var names=['name1','name2','name3','name4'];
     var list='';
-    for(var i=0; i<name.length;i++){
+    for(var i=0; i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul= document.getElementById('namelist');
